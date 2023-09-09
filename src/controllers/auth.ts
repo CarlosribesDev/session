afd9208e2ed7920f712392448756c88da1719bd7
CarlosribesDev <carlosribes.dev@gmail.com>
2023-09-09 05:46:52 +0200

    feat: devolver token

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,19 +3,27 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import User from '../model/user';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const login = asyncHandler(async (req: Request, res: Response) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
   
-  const authCorrect = user === null ? false : await bcrypt.compare(password, user.password);  
+  const authCorrect = !user ? false : await bcrypt.compare(password, user.password);  
   
-  if (!authCorrect) {
+  if (!user || !authCorrect) {
     res.status(StatusCodes.UNAUTHORIZED).send({ message: 'Invalid username or password' });
     return;
   }
+
+  const userForToken = {
+    id: user._id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET_KEY || 'secret');
   
-  res.status(StatusCodes.OK).send(user);
+  res.status(StatusCodes.OK).send(token);
 });
 
 export default { login };
